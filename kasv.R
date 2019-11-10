f899f9b9c723b2fee6fdd0f90ed8677b43a82296
Taavi Päll <tapa741@gmail.com>
2019-11-10 10:01:03 +0200

    added fig

--- kasv.R
+++ kasv.R
@@ -3,4 +3,21 @@ library(here)
 library(lubridate)
 
 pikkused <- read_csv(here::here("pikkused.csv"), 
-                     col_types = cols(kuupäev = col_date(format = "%d%m%Y")))
+                     col_types = cols(kuupäev = col_date(format = "%d-%m-%Y")))
+
+pikkused %>% 
+  ggplot() +
+  geom_line(aes(kuupäev, pikkus)) +
+  facet_wrap(~laps, scales = "free_x")
+
+pikkused <- pikkused %>% 
+  mutate(vanus = case_when(
+    laps == "KM" ~ interval(dmy("27-08-2002"), kuupäev) / years(1),
+    laps == "A" ~ interval(dmy("13-04-2013"), kuupäev)  / dyears(1)
+  ))
+
+pikkused %>% 
+  ggplot() +
+  geom_line(aes(vanus, pikkus / 1000, color = laps)) +
+  labs(y = "Pikkus, m", x = "Vanus, aastat")
+
